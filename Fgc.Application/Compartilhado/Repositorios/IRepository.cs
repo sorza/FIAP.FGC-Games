83831e8f920d33742053dda0845bacac9967c1ea
sorza <alexandre.sorza@outlook.com>
2025-07-15 17:43:33 -0300

    Criando repositorio generico

---
+++ Fgc.Application/Compartilhado/Repositorios/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Fgc.Domain.Compartilhado.Entidades;
+
+namespace Fgc.Application.Compartilhado.Repositorios
+{
+    public interface IRepository<T> where T : Entidade
+    {
+        IList<T> ObterTodos();
+        IList<T> ObterPorId(Guid id);
+        Task Cadastrar(T entidade);
+        Task Alterar(T entidade);
+        Task Deletar(Guid id);
+    }
+}
