6c6efc13f6e7904097931e628012963eb2f63fd5
sorza <alexandre.sorza@outlook.com>
2025-07-15 18:15:02 -0300

    Implementando o repositorio generico

--- Fgc.Application/Compartilhado/Repositorios/IRepository.cs
+++ Fgc.Application/Compartilhado/Repositorios/IRepository.cs
@@ -5,7 +5,7 @@ namespace Fgc.Application.Compartilhado.Repositorios
     public interface IRepository<T> where T : Entidade
     {
         IList<T> ObterTodos();
-        IList<T> ObterPorId(Guid id);
+        T ObterPorId(Guid id);
         Task Cadastrar(T entidade);
         Task Alterar(T entidade);
         Task Deletar(Guid id);
---
+++ Fgc.Infrastructure/Compartilhado/Repositorios/EFRepository.cs
@@ -0,0 +1,50 @@
+﻿using Fgc.Application.Compartilhado.Repositorios;
+using Fgc.Domain.Compartilhado.Entidades;
+using Fgc.Infrastructure.Compartilhado.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fgc.Infrastructure.Compartilhado.Repositorios
+{
+    public class EFRepository<T>(AppDbContext context) : IRepository<T> where T : Entidade
+    {
+        protected readonly DbSet<T> _dbSet = context.Set<T>();
+
+        public Task Alterar(T entidade)
+        {
+            if (entidade is null) throw new ArgumentNullException(nameof(entidade));
+            
+            entidade.AtualizarDataAlteracao();
+
+            _dbSet.Update(entidade);
+            return context.SaveChangesAsync();
+        }
+
+        public Task Cadastrar(T entidade)
+        {
+            if (entidade is null) throw new ArgumentNullException(nameof(entidade));
+            
+            _dbSet.Add(entidade);
+            return context.SaveChangesAsync();
+        }
+
+        public Task Deletar(Guid id)
+        {
+            var entidade = ObterPorId(id);
+
+            _dbSet.Remove(entidade);
+            return context.SaveChangesAsync();
+
+        }
+
+        public T ObterPorId(Guid id)
+        {
+            if (id == Guid.Empty) throw new ArgumentException("O ID não pode ser vazio.", nameof(id));
+            var entidade = _dbSet.Find(id);
+            if (entidade is null) throw new KeyNotFoundException($"Entidade com ID {id} não encontrada.");
+            return entidade;
+        }
+
+        public IList<T> ObterTodos() => _dbSet.ToList();
+        
+    }
+}
