ed1d6a7ce9ee0b574d0390fb1099ef12ff238b89
sorza <alexandre.sorza@outlook.com>
2025-07-16 19:57:45 -0300

    Injetando dependencia do repositorio generico

--- Fgc.Infrastructure/Compartilhado/DependencyInjection.cs
+++ Fgc.Infrastructure/Compartilhado/DependencyInjection.cs
@@ -1,6 +1,20 @@
-﻿namespace Fgc.Infrastructure.Compartilhado
+﻿using Fgc.Application.Biblioteca.Repositorios;
+using Fgc.Application.Compartilhado.Repositorios.Abstracoes;
+using Fgc.Domain.Biblioteca.Entidades;
+using Fgc.Infrastructure.Biblioteca.Repositorios;
+using Fgc.Infrastructure.Compartilhado.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fgc.Infrastructure.Compartilhado
 {
     public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            // Repositories
+            services.AddScoped<IRepository<Genero>, EFRepository<Genero>>();
+
+            return services;
+        }
     }
 }
