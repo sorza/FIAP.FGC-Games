1e8cac5455f5c7c35e3fb7d26c715282b13fabf0
sorza <alexandre.sorza@outlook.com>
2025-07-16 19:49:35 -0300

    Defininfo Command e Response para criacao de um Genero

---
+++ Fgc.Application/Biblioteca/CasosDeUso/Genero/Criar/Command.cs
@@ -0,0 +1,6 @@
+﻿using Fgc.Application.Compartilhado.CasosDeUso.Abstracoes;
+
+namespace Fgc.Application.Biblioteca.CasosDeUso.Genero.Criar
+{
+    public sealed record Command(string nomeGenero) : ICommand<Response>;
+}
---
+++ Fgc.Application/Biblioteca/CasosDeUso/Genero/Criar/Response.cs
@@ -0,0 +1,6 @@
+﻿using Fgc.Application.Compartilhado.CasosDeUso.Abstracoes;
+
+namespace Fgc.Application.Biblioteca.CasosDeUso.Genero.Criar
+{
+    public sealed record Response(Guid Id, string Genero) : ICommandResponse;
+}
