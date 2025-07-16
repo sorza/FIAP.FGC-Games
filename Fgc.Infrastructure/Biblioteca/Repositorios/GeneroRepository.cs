c582583c90790745e6a4f1e8b2d51b891e186029
sorza <alexandre.sorza@outlook.com>
2025-07-16 10:25:39 -0300

    Criando repositorio de Genero e Jogo

---
+++ Fgc.Infrastructure/Biblioteca/Repositorios/GeneroRepository.cs
@@ -0,0 +1,13 @@
+﻿using Fgc.Domain.Biblioteca.Entidades;
+using Fgc.Infrastructure.Compartilhado.Data;
+using Fgc.Infrastructure.Compartilhado.Repositorios;
+
+namespace Fgc.Infrastructure.Biblioteca.Repositorios
+{
+    public class GeneroRepository : EFRepository<Genero>
+    {
+        public GeneroRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Fgc.Infrastructure/Biblioteca/Repositorios/JogoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Fgc.Domain.Biblioteca.Entidades;
+using Fgc.Infrastructure.Compartilhado.Data;
+using Fgc.Infrastructure.Compartilhado.Repositorios;
+
+namespace Fgc.Infrastructure.Biblioteca.Repositorios
+{
+    public class JogoRepository : EFRepository<Jogo>
+    {
+        public JogoRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
