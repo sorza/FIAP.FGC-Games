05fc25d89cfb9b7107460a7b88af327cf0a3eb43
sorza <alexandre.sorza@outlook.com>
2025-07-21 13:01:40 -0300

    Criando um LoggingBehavior

---
+++ Fgc.Application/Compartilhado/Comportamentos/LoggingBehavior.cs
@@ -0,0 +1,33 @@
+﻿using Fgc.Application.Compartilhado.CasosDeUso.Abstracoes;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Fgc.Application.Compartilhado.Comportamentos
+{
+    public class LoggingBehavior<TRequest, TResponse>(ILogger<TRequest> logger)
+       : IPipelineBehavior<TRequest, TResponse> where TRequest : ICommand
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            try
+            {
+                logger.LogInformation($"Executando requisição: {request.GetType().Name}");
+                var result = await next();
+
+                stopwatch.Stop();
+
+                logger.LogInformation($"Requisição {request.GetType().Name} processada em {stopwatch.ElapsedMilliseconds}ms.");
+                return result;
+            }
+            catch (Exception e)
+            {
+                stopwatch.Stop();
+                logger.LogError(e, $"Ocorreu um erro durante o processamento da requisição: {request.GetType().Name} depois de {stopwatch.ElapsedMilliseconds}ms");
+                throw;
+            }
+        }
+    }
+}
