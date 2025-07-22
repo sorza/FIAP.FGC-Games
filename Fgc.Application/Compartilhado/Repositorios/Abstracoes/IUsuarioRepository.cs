5e338933d31387435d106efc9eadd2be6e5a585b
sorza <alexandre.sorza@outlook.com>
2025-07-22 19:49:54 -0300

    Implementando repositorio para Usuario

---
+++ Fgc.Application/Compartilhado/Repositorios/Abstracoes/IUsuarioRepository.cs
@@ -0,0 +1,6 @@
+﻿using Fgc.Domain.Usuario.Entidades;
+
+namespace Fgc.Application.Compartilhado.Repositorios.Abstracoes
+{
+    public interface IUsuarioRepository : IRepository<Conta>;
+}
---
+++ Fgc.Infrastructure/Usuario/Repositorios/UsuarioRepository.cs
@@ -0,0 +1,17 @@
+﻿using Fgc.Application.Compartilhado.Repositorios.Abstracoes;
+using Fgc.Domain.Biblioteca.Entidades;
+using Fgc.Domain.Usuario.Entidades;
+using Fgc.Infrastructure.Compartilhado.Data;
+using Fgc.Infrastructure.Compartilhado.Repositorios;
+
+namespace Fgc.Infrastructure.Usuario.Repositorios
+{
+    public class UsuarioRepository : GenericoRepository<Conta>, IUsuarioRepository
+    {
+        private readonly AppDbContext _context;
+        public UsuarioRepository(AppDbContext context) : base(context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+    }
+}
