fb12ae7d768791210c52477bea26d761b8f1e698
sorza <alexandre.sorza@outlook.com>
2025-08-21 23:12:45 -0300

    Removendo dev mode

--- Fgc.Api/Program.cs
+++ Fgc.Api/Program.cs
@@ -142,16 +142,12 @@ app.UseSerilogRequestLogging(opts =>
     };
 });
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fgc.Api v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fgc.Api v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseAuthentication();
 app.UseAuthorization();
